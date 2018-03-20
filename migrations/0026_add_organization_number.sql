82f404ad6f9a128aa49a77fd7f09f8923079f36a
Hänniz <johannes.eschricht@gmail.com>
2018-03-20 11:46:36 +0100

    added Organization number to organizations in DB and API

---
+++ migrations/0026_add_organization_number.sql
@@ -0,0 +1,27 @@
+
+
+-- +migrate Up
+alter table organization
+    add column org_nr character varying (100);
+
+insert into organization (
+	created_at,
+	updated_at,
+	name,
+	display_name,
+	can_have_gateways,
+	org_nr
+) values(
+	now(),
+	now(),
+	'loraserver',
+	'LoRa Server',
+	true,
+	'19891110'
+);
+
+-- +migrate Down
+alter table organization
+    drop column org_nr;
+
+-- +migrate StatementEnd
